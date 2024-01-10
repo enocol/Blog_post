39fc8bdabaed5587032663d5ebdf28fc4282d636
Enoh <eno.col@gmail.com>
2024-01-10 14:18:41 +0000

    Fix linters

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 20_240_110_083_418) do
-  # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
   create_table 'comments', force: :cascade do |t|
